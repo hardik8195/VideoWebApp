7738b08a4ba9b6da7fcb4820a0cb23ead90f0ff9
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-27 10:12:06 +0530

    updated

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
 
-dotenv.config();
 const app = express();
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
--- server-side/src/db/index.js
+++ server-side/src/db/index.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 import { DB_NAME} from "../constants.js";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 
 export default async function connectDB(){
