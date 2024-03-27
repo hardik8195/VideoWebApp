f79423b8cee982541416b15d8ba24c37d25dd375
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-27 12:13:06 +0530

    updated

--- server-side/src/utils/cloudinary.js
+++ server-side/src/utils/cloudinary.js
@@ -2,9 +2,7 @@ import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs'
 import dotenv from 'dotenv';
 
-dotenv.config({
-  path: './.env'
-})
+dotenv.config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
