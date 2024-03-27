4477b569cb6d833fa05c7f0c58c521643010759d
hardik8195 <154544538+hardik8195@users.noreply.github.com>
2024-03-27 20:24:33 +0530

    Update app.js

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin:"https://video-tube-pj7mhrjrs-bondman8195s-projects.vercel.app",
     credentials:true
 }))
 
