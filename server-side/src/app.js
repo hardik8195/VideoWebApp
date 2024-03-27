9a8150848d6b94820b5b88a340cf9f3b93d370a8
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-27 13:06:41 +0530

    updated

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -4,8 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors({
-    origin:"https://you-tube-hk3soxpsd-bondman8195s-projects.vercel.app",
-    methods : ["POST","GET","DELETE","PUT","PATCH"],
+    origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
 
