97176601519686a5a8702c91f33f68be0d704314
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-30 09:34:00 +0530

    cors

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'
-import path from 'path'
+import cors from 'cors'
 
 dotenv.config({
     path: './.env'
