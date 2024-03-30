db77008155b3413bb1e36f4b009ddb49c9fc571f
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-30 09:31:35 +0530

    updated

--- server-side/src/app.js
+++ server-side/src/app.js
@@ -9,21 +9,14 @@ dotenv.config({
 
 const app = express();
 
-
-
-
-const allowCrossDomain = (req, res, next) => {
-  res.header(`Access-Control-Allow-Origin`, `http://localhost:5173`);
-  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
-  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-};
+app.use(cors({
+  origin:process.env.CORS_ORIGIN,
+  credentials:true
+}))
 
 
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
-app.use(allowCrossDomain);
 app.use(express.static("public"))
 app.use(cookieParser())
 
